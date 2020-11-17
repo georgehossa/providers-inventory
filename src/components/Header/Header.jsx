f312420decac8d5aa4bbdd91113731b72e18f9cf
Jorge Ossa <jorgehossa@gmail.com>
2020-11-16 21:40:11 -0500

    Add header components and project logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import ProjectInfo from '../ProjectInfo';
+import NavBar from '../NavBar';
 import PropTypes from 'prop-types';
-//import { Test } from './Header.styles';
+import { Container, Wrapper, BackButton } from './Header.styles';
+import Logo from '../../assets/terrazino-cumbres.png';
 
-const Header = (props) => (
-  <div className="HeaderWrapper">
-    Header
-  </div>
+const Header = ({project}) => (
+  <Container>
+    <Wrapper>
+      <BackButton />
+      <ProjectInfo project={project}/>
+      <NavBar/>
+    </Wrapper>
+  </Container>
 );
 
 Header.propTypes = {
-  // bla: PropTypes.string,
+  project: PropTypes.any,
 };
 
 Header.defaultProps = {
-  // bla: 'test',
+  project: {
+    name: 'Terrazzino cumbres',
+    city: 'Envigado',
+    address: 'Carrera 43A No. 49B Sur - 82',
+    logo: {Logo},
+  },
 };
 
 export default Header;
--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,6 +1,33 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
-// export const Test = styled.div`
-//  display: flex;
-// `;
-//
+export const Container = styled.header`
+  display: block;
+  background-color: white;
+  border-bottom: 2px solid ${props => props.theme.colorNeutral};
+`;
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template: auto / 70px 1fr .5fr;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem 0;
+  width: 90vw;
+  margin: 0 auto;
+`
+const size = '2.5rem';
+
+export const BackButton = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${size};
+  height: ${size};
+  border-radius: 50%;
+  box-shadow: 0 0 15px -3px ${props => props.theme.colorDefault};
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' class='icon icon-tabler icon-tabler-chevron-left' width='24' height='24' stroke-width='1.5' stroke='%23012d26' fill='none' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath d='M0 0h24v24H0z' stroke='none'/%3E%3Cpath d='M15 6l-6 6 6 6'/%3E%3C/svg%3E");
+  background-repeat: no-repeat;
+  background-position: center;
+  cursor: pointer;
+`
+
