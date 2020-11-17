9f856725fb1bb90e904af14417edbe2004679fdb
Jorge Ossa <jorgehossa@gmail.com>
2020-11-16 23:26:01 -0500

    Style menu component

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-//import { Test } from './Menu.styles';
+import { Container, MenuTrigger, Avatar } from './Menu.styles';
 
-const Menu = (props) => (
-  <div className="MenuWrapper">
-    Menu
-  </div>
-);
+const Menu = ({forwardedRef}) => {
+  return (
+    <Container ref={forwardedRef}>
+      <MenuTrigger>
+        <span>Menu</span>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" strokeWidth="1.5" stroke="#012d26" fill="none" strokeLinecap="round" strokeLinejoin="round">
+          <path stroke="none" d="M0 0h32v32H0z" fill="none"/>
+          <line x1="0" y1="6" x2="20" y2="6" />
+          <line x1="0" y1="12" x2="15" y2="12" />
+          <line x1="0" y1="18" x2="10" y2="18" />
+          <line x1="0" y1="24" x2="8" y2="24" />
+        </svg>
+      </MenuTrigger>
+      <Avatar>
+        JH
+      </Avatar>
+    </Container>
+  );
+
+}
 
 Menu.propTypes = {
   // bla: PropTypes.string,
--- src/components/Menu/Menu.styles.js
+++ src/components/Menu/Menu.styles.js
@@ -1,6 +1,44 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
-// export const Test = styled.div`
-//  display: flex;
-// `;
-//
+export const Container = styled.aside`
+  position: fixed;
+  left: 0;
+  top: 0;
+  display: flex;
+  width: 70px;
+  height: 100vh;
+  padding: 1.5rem .5rem;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${props => props.theme.colorBrand};
+`;
+
+export const MenuTrigger = styled.div`
+  display: flex;
+  justify-content: center;
+
+  flex-direction: column;
+  color: ${props => props.theme.colorSecondary};
+  font-size: .6rem;
+  text-transform: uppercase;
+  letter-spacing: .2rem;
+  cursor: pointer;
+  font-weight: 700;
+  svg {
+    display: block;
+    width: 100%;
+  }
+`
+const size = '2rem';
+export const Avatar = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: ${size};
+  height: ${size};
+  border-radius: 50%;
+  background-color: white;
+  font-weight: 700;
+  cursor: pointer;
+`
