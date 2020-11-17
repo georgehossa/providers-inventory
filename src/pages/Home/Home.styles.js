95ba0c32981448d4eb973336ba48aadf25e96bcc
Jorge Ossa <jorgehossa@gmail.com>
2020-11-16 23:30:25 -0500

    Improve Home layout Refactor home styles to add menu component

--- src/pages/Home/Home.styles.js
+++ src/pages/Home/Home.styles.js
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 
 export const Layout = styled.main`
   display: grid;
-  grid-template: auto auto / auto 1fr;
-  grid-template-areas: "nav header" "nav content";
+  grid-template: auto auto / 1fr;
+  grid-template-areas: "header" "content";
+  padding-left: 70px;
   color: ${props => props.theme.mainColor};
   background-color: ${props => props.theme.colorNeutral};
+  min-height: 100vh;
 `;
